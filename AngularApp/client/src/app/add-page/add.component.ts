12f7e62081cec2107becbdf5dcaa04fbbb265f8b
Matis Grégoire <matis.gregoire@polymtl.ca>
2022-11-14 17:08:28 -0500

    MG: change home buttons to Ajouter, Modifier and Supprimer

---
+++ AngularApp/client/src/app/add-page/add.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "add-page",
+    templateUrl: "./add.component.html",
+    styleUrls: ["./add.component.css"],
+})
+export class AddComponent implements OnInit {
+    public route: string;
+
+    public ngOnInit(): void { }
+}
--- AngularApp/client/src/app/modules/app-routing.module.ts
+++ AngularApp/client/src/app/modules/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "../app.component";
+import { AddComponent } from "../add-page/add.component";
 
 const routes: Routes = [
-  { path: "app", component: AppComponent }
+  { path: "app", component: AppComponent },
+  { path: "add", component: AddComponent }
 ];
 
 @NgModule({
